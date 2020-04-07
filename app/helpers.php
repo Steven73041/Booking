676901fe3bdbb102c2de0e3f6043cfc35099e8de
Steven73041 <mclaren730@gmail.com>
2020-04-07 16:15:05 +0300

    remove test storage

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Storage;
 
 if (!function_exists('get_image_path')) {
 	function get_image_path($image_name) {
-		return asset('storage/'.$image_name);
+		return asset($image_name);
 //		return Storage::disk('public')->url($image_name);
 	}
 }
