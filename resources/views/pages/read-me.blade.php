7a80efe659123da8861fc0ddb6a87c60ad985775
Steven73041 <mclaren730@gmail.com>
2020-04-10 22:56:36 +0300

    Added more in read-me page

--- resources/views/pages/read-me.blade.php
+++ resources/views/pages/read-me.blade.php
@@ -14,13 +14,23 @@
                     <li>{{__('Stripe payment')}}</li>
                     <li>{{__('Add/Edit/Delete Reviews')}}</li>
                 </ul>
+                <h3>{{__('Hidden highlights')}}</h3>
+                <ul class="mb-4">
+                    <li>{{__('Image class cropping images to 450*450')}}</li>
+                    <li>{{__('Session setting/unsetting for all critical requests (payments, bookings etc.)')}}</li>
+                    <li>{{__('Strict security policies for CRUD based on ownership of object')}}</li>
+                    <li>{{__('Weather "widget" by open API based on IP location, using FontAwesome for icons')}}</li>
+                    <li>{{__('Using bootsrap at 100% of building front-end before adding custom SASS')}}</li>
+                    <li>{{__('Using animate.js for some "not so" beautiful animations.')}}</li>
+                </ul>
                 <h3>{{__('Payments')}}</h3>
                 <h4>{{__('Test Cards:')}}</h4>
                 <ul class="mb-4">
                     <li>{{__('Success: ')}}<code>4242 4242 4242 4242</code></li>
                 </ul>
                 <h2>{{__('Credits')}}</h2>
-                <p>{{__('Made by ')}}<strong>{{get_author()}}</strong>{{__('using jQuery, SASS and every single feature of Laravel 7.x')}}</p>
+                <p>{{__('Made by ')}}<strong>{{get_author()}}</strong>{{__(' using jQuery, SASS and every single feature of ')}}<a href="https://laravel.com/">{{__('Laravel 7.x')}}</a></p>
+                <p></p>
             </div>
         </div>
     </div>
