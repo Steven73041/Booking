856db111d8df400a5d68cb4e77544f72058dbb0c
Steven73041 <mclaren730@gmail.com>
2020-04-10 22:41:08 +0300

    update page

--- resources/views/pages/read-me.blade.php
+++ resources/views/pages/read-me.blade.php
@@ -1,12 +1,12 @@
 @extends('home-layout')
 @section('PageTitle', $title?:'Read Me')
 @section('content')
-    <div class="content">
+    <div class="container">
         <div class="row">
             <div class="col-sm-12">
                 <h1>{{__('Read Me')}}</h1>
-                <h2>{{__('Highlights')}}</h2>
-                <ul>
+                <h3>{{__('Highlights')}}</h3>
+                <ul class="mb-4">
                     <li>{{__('Upload/Edit Rooms gallery')}}</li>
                     <li>{{__('Create/Edit User')}}</li>
                     <li>{{__('Booked dates disabled in calendar of room')}}</li>
@@ -14,11 +14,13 @@
                     <li>{{__('Stripe payment')}}</li>
                     <li>{{__('Add/Edit/Delete Reviews')}}</li>
                 </ul>
-                <h2>{{__('Payments')}}</h2>
-                <h3>{{__('Test Cards:')}}</h3>
-                <ul>
+                <h3>{{__('Payments')}}</h3>
+                <h4>{{__('Test Cards:')}}</h4>
+                <ul class="mb-4">
                     <li>{{__('Success: ')}}<code>4242 4242 4242 4242</code></li>
                 </ul>
+                <h2>{{__('Credits')}}</h2>
+                <p>{{__('Made by <strong>Anastasis Mastoris</strong> using jQuery, SASS and every single feature of Laravel 7.x')}}</p>
             </div>
         </div>
     </div>
